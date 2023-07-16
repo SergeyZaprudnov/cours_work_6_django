8f6bfe355853f1f0ee72a62dd29a87a5cd3322ea
sergey <szaprudnov@hotmail.com>
2023-07-16 10:55:23 +0300

    creating a client model

--- my_work/models.py
+++ my_work/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):  # Модель Клиента
+    name = models.CharField(max_length=100, verbose_name='ФИО')
+    email = models.EmailField(verbose_name='Электронная почта')
+    comment = models.CharField(max_length=200, verbose_name='Комментарий')
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
