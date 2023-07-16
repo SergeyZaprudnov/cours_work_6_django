8abe396b028ffeea790c0ae9ce8de54dee681418
sergey <szaprudnov@hotmail.com>
2023-07-16 14:59:53 +0300

    add

--- my_work/urls.py
+++ my_work/urls.py
@@ -14,15 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('my_work.urls', namespace='my_work')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+app_name = 'my_work'
