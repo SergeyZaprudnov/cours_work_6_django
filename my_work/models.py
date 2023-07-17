32302ab2c4d19221db34ea50664299f3d29d7aec
sergey <szaprudnov@hotmail.com>
2023-07-17 22:41:17 +0300

    editing model Client

--- my_work/models.py
+++ my_work/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Client(models.Model):  # Модель Клиента
@@ -6,3 +7,18 @@ class Client(models.Model):  # Модель Клиента
     email = models.EmailField(verbose_name='Электронная почта')
     comment = models.CharField(max_length=200, verbose_name='Комментарий')
     is_active = models.BooleanField(default=True, verbose_name='Активность')
+
+    def get_absolute_url(self):
+        return reverse('my_work:user', args=[self.pk])
+    def delete(self, **kwargs):
+        self.is_active = False
+        self.save()
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}, {self.email}, {self.comment}, {self.is_active}'
+
