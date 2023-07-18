5fc9e936103ea789e740eb9e804863c0f12d5723
sergey <szaprudnov@hotmail.com>
2023-07-18 23:01:30 +0300

    admin panel setup

--- my_work/admin.py
+++ my_work/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from my_work.models import Client, Setting, Messages, Logs
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'email', 'comment', 'is_active')
+
+
+@admin.register(Setting)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ('mailing_name', 'message', 'user_name', 'date_mailing', 'date_end_mailing', 'is_active')
+    filter_horizontal = ('user_name',)
+
+
+@admin.register(Messages)
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ('theme', 'body', 'is_active')
+
+
+@admin.register(Logs)
+class LogsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_last', 'status', 'answer')
+    list_filter = ('settings_mailing_name',)
