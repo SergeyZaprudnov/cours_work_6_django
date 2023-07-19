86fd170e8e35abb9e5f4868247f836dde00a5dd9
sergey <szaprudnov@hotmail.com>
2023-07-19 10:52:19 +0300

    class description

--- my_work/views.py
+++ my_work/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.timezone import now
 from django.views import generic
 
-from my_work.form import UserCreateForm, SettingCreateForm
-from my_work.models import Client, Setting
+from my_work.form import UserCreateForm, SettingCreateForm, MessageCreateForm
+from my_work.models import Client, Setting, Messages, Logs
 
 
 def start_page(request):
@@ -91,8 +92,51 @@ class SettingsUpdateView(generic.UpdateView):
     form_class = SettingCreateForm
     success_url = reverse_lazy('my_work:setting_list')
 
+
 class SettingsDeleteView(generic.DeleteView):
     model = Setting
     template_name = 'settings/setting_delete.html'
     success_url = reverse_lazy('my_work:setting_list')
 
+
+class MessageListView(generic.ListView):
+    model = Messages
+    template_name = 'message/message_list.html'
+
+
+class MessageDetailView(generic.DetailView):
+    model = Messages
+    template_name = 'message/message_detail.html'
+
+
+class MessageCreateView(generic.CreateView):
+    model = Messages
+    template_name = 'message/message_create.html'
+    form_class = MessageCreateForm
+    success_url = reverse_lazy('my_work:message_list')
+
+
+class MessageDeleteView(generic.DeleteView):
+    model = Messages
+    template_name = 'message/message_delete.html'
+    success_url = reverse_lazy('my_work:message_list')
+
+
+def start_mailing(request, pk):
+    context = {'result': 'Рассылка запущена'}
+
+    data = Setting.objects.get(id=pk)
+    Logs.objects.create(
+        title=f'Запуск: {data.mailing_name}',
+        date_last=now(),
+        status='Рассылка включена',
+        answer='Без ответа',
+        settings=data,
+    )
+
+    a = AutoMail(data)
+    return render(request, 'my_work:a_mail', context=context)
+
+class LogListView(generic.ListView):
+    model = Logs
+    template_name = 'log.html'
