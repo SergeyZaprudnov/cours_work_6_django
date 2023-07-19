f9522d3b722e5684dfe4945495a22d28de110c9f
sergey <szaprudnov@hotmail.com>
2023-07-19 10:33:31 +0300

    class description

--- my_work/form.py
+++ my_work/form.py
@@ -1,5 +1,30 @@
 from django import forms
 
+from my_work.models import Client, Setting, Messages
+
 
 class UserCreateForm(forms.ModelForm):
-    pass
+    name = forms.CharField(label='ФИО')
+    email = forms.EmailField(label='email')
+    comment = forms.CharField(label='Комментарий')
+
+    class Meta:
+        model = Client
+        fields = ('name', 'email', 'comment')
+
+
+class SettingCreateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['status'].widget = forms.HiddenInput()
+
+    class Meta:
+        model = Setting
+        fields = '__all__'
+
+
+class MessageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Messages
+        fields = '__all__'
